1a4578164356ef3d713a631948a64f057de290bb
Michael Goff <msgoff@users.noreply.github.com>
2018-10-13 20:03:58 +0000

    Update snake.sql

--- snake.sql
+++ snake.sql
@@ -1,68 +1,70 @@
 --time for ((i=0;i<1000;i++));do psql -f /tmp/s2.sql ;done|grep "{"|sort|uniq|sed -re 's/\{|\}|\(|\)//g'
 
-drop FUNCTION if EXISTS transition_table;
-DROP TABLE if EXISTS transition_table;
-drop function if EXISTS get_moves;
-drop table if EXISTS seen;
-drop function if exists get_src;
-drop function if exists get_last_src;
-drop function if exists random_move;
-drop function if exists update_transition_table;
+
+DROP FUNCTION IF EXISTS transition_table;
+DROP TABLE IF EXISTS transition_table;
+DROP FUNCTION IF EXISTS get_moves;
+DROP TABLE IF EXISTS seen;
+DROP FUNCTION IF EXISTS get_src;
+DROP FUNCTION IF EXISTS get_last_src;
+DROP FUNCTION IF EXISTS random_move;
+DROP FUNCTION IF EXISTS update_transition_table;
+DROP FUNCTION IF EXISTS create_random_move;
 
 
-create function transition_table(int,int)
-returns int[]
+
+CREATE FUNCTION transition_table(int,int)
+RETURNS INT[]
 language sql immutable
-as
-$$
+as $$
 SELECT ARRAY(
-SELECT * from UNNEST(
-(SELECT array[
-(SELECT $1-$2 where $1 - $2 > 0),
-(SELECT $1-1 where ($1 - 1) % $2 <> 0 ),
-(SELECT $1+1 where $1 % $2 <> 0),
-(SELECT $1+$2 where $1 + $2 < $2*$2)
-])) as X
-where X is not NULL
+  SELECT * from UNNEST(
+        (SELECT array[
+        (SELECT $1-$2 WHERE $1 - $2 > 0),
+        (SELECT $1-1 WHERE ($1 - 1) % $2 <> 0 ),
+        (SELECT $1+1 WHERE $1 % $2 <> 0),
+        (SELECT $1+$2 WHERE $1 + $2 < $2*$2)
+        ])) as X
+        WHERE X IS NOT NULL
 );
 $$;
 
 
-create table transition_table as
-select ix,transition_table(ix,3) AS arr
-from generate_series(1,9) as ix;
+CREATE TABLE transition_table
+  AS
+    SELECT ix,transition_table(ix,3) AS ARR
+    FROM generate_series(1,9) AS ix;
+
+CREATE TABLE seen(id SERIAL,src INT);
 
 
-create table seen(id serial,src int);
 
-
-create function get_moves(int)
-returns int[]
+CREATE FUNCTION get_moves(INT)
+RETURNS INT[]
 LANGUAGE SQL
-as $$
-  select array((
-  select * from UNNEST((
-  select arr from transition_table
-  where ix = $1)) as X
-  where X not in (select src from seen)))
-
+AS $$
+  SELECT array((
+  SELECT * FROM UNNEST((
+  SELECT arr FROM transition_table
+  WHERE ix = $1)) AS X
+  WHERE X NOT IN (SELECT src FROM seen)))
 $$;
 
-create function get_src()
+
+
+CREATE FUNCTION get_src()
   returns INT
   LANGUAGE SQL
-  as $$
-
-    select ix from transition_table
-    where ix not in (select src from seen )
-    order by random()
-
-    limit 1
-
-  $$;
+  AS $$
+    SELECT ix FROM transition_table
+    WHERE ix NOT IN (SELECT src FROM seen )
+    ORDER BY random()
+    LIMIT 1
+$$;
 
 
-insert into seen (src) values  ((select get_src()));
+INSERT INTO seen (src) VALUES ((SELECT get_src()));
+
 
 create function get_last_src()
   returns int language sql
@@ -75,49 +77,43 @@ $$;
 create function random_move()
 returns INT
 LANGUAGE SQL
-as
-$$
+as $$
 select * from UNNEST((
 SELECT get_moves((
-select get_last_src()
-))))
-order by random()
+select get_last_src() ))
+)) ORDER BY random()
 limit 1;
 $$;
 
-
 create function update_transition_table()
-  returns void
+  RETURNS VOID
   LANGUAGE SQL
-  as
-  $$
-  delete from transition_table
+AS $$
+  DELETE FROM transition_table
     where ix in (select src from seen)
+$$;
+
+CREATE FUNCTION create_random_move()
+  RETURNS VOID
+  LANGUAGE SQL
+  AS $$
+  insert into seen (src) values ((select random_move()));
   $$;
 
 
-insert into seen (src) values  ((select random_move()));
-insert into seen (src) values  ((select random_move()));
-insert into seen (src) values  ((select random_move()));
-insert into seen (src) values  ((select random_move()));
-insert into seen (src) values  ((select random_move()));
-insert into seen (src) values  ((select random_move()));
-insert into seen (src) values  ((select random_move()));
-insert into seen (src) values  ((select random_move()));
+SELECT create_random_move() FROM generate_series(1,8);
 
+DELETE FROM seen
+  WHERE src IS NULL;
 
-delete from seen
-  where src is Null;
+SELECT * FROM seen;
 
-select * from seen;
-
-select (
+SELECT (
   CASE
-  when (select count(*) from seen) = 9
-  Then (select array_agg(s) from (select src from seen) as s)
-  ELSE (select array_agg(s) from (select * from generate_series(0,0)) as s)
+  WHEN (SELECT count(*) FROM seen) = 9
+  THEN (select array_agg(s) FROM (SELECT src FROM seen) AS s)
+  ELSE (select array_agg(s) FROM (SELECT 0) AS s)
   End
-) as f
-from seen
-limit 1
-;
+) AS f
+FROM seen
+LIMIT 1;
